49416a7243c7ea2284e027d54d9e795c7b5c240c
Borja Martí <borjamarti@outlook.com>
2024-06-04 00:08:25 +0200

    fixed page export

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import styles from "./page.module.css";
 import { Header } from "./components/header";
 import { Hero } from "./components/hero";
 
-export const Home = () => {
+export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
       <Hero />
     </main>
   );
-};
-
-export default Home;
+}
