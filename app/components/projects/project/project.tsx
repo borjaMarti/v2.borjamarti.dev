b772d5580c522becee834f087284792ed780c6ab
Borja Martí <borjamarti@outlook.com>
2024-06-08 23:59:08 +0200

    progressed project

--- app/components/projects/project/project.tsx
+++ app/components/projects/project/project.tsx
@@ -1,3 +1,17 @@
-export const Project = () => {
-  return <article></article>;
+import Image from "next/image";
+
+interface ProjectProps {
+  title: string;
+  src: string;
+}
+
+export const Project = ({ title, src }: ProjectProps) => {
+  return (
+    <article>
+      <div>
+        <Image src={src} alt={`${title} `} />
+      </div>
+      <h3>{title}</h3>
+    </article>
+  );
 };
